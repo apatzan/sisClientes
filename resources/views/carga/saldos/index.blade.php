e89cedd765606b07f35e9778c606bfa8a34a7c77
apatzan <angelpatzan55@gmail.com>
2026-09-16 18:49:14 +0200

    Add loading spinner to the cargar saldos view

--- resources/views/carga/saldos/index.blade.php
+++ resources/views/carga/saldos/index.blade.php
@@ -7,7 +7,7 @@
 
 
 
-     <form method="post" enctype="multipart/form-data" action="{{ url('/carga/saldos/import') }}">
+     <form id="form-carga-saldos" method="post" enctype="multipart/form-data" action="{{ url('/carga/saldos/import') }}">
     {{ csrf_field() }}
     <div class="form-group">
      <table class="table">
@@ -17,7 +17,7 @@
         <input type="file" name="select_file"  accept=".txt" required="true" />
        </td>
        <td width="30%" align="left">
-        <input type="submit" name="upload" class="btn btn-primary" value="Cargar Información">
+        <input type="submit" name="upload" id="btn-cargar-saldos" class="btn btn-primary" value="Cargar Información">
        </td>
       </tr>
       <tr>
@@ -32,8 +32,52 @@
    @if (session('report'))
     <div class="alert alert-info">{{ session('report') }}</div>
     @endif
-   </form>
-            
+
     </div>
   </div>
+
+  <div id="loading-overlay-saldos" style="display:none;">
+   <div class="loading-overlay-content">
+    <i class="fa fa-spinner fa-spin"></i>
+    <p>Cargando información, por favor espere...</p>
+   </div>
+  </div>
+
+  <style>
+   #loading-overlay-saldos {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.85);
+    z-index: 9999;
+    text-align: center;
+   }
+   #loading-overlay-saldos .loading-overlay-content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+   }
+   #loading-overlay-saldos .fa-spinner {
+    font-size: 48px;
+    color: #337ab7;
+   }
+   #loading-overlay-saldos p {
+    margin-top: 15px;
+    font-size: 16px;
+    color: #333;
+   }
+  </style>
+
+  <script>
+   document.getElementById('form-carga-saldos').addEventListener('submit', function (e) {
+    if (!this.checkValidity()) {
+     return;
+    }
+    document.getElementById('btn-cargar-saldos').disabled = true;
+    document.getElementById('loading-overlay-saldos').style.display = 'block';
+   });
+  </script>
 @endsection
